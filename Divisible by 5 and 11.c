dd9bae80b7f794151af69f6289f63fb516d5ba7c
vamshikumar123 <59007250+vamshikumar123@users.noreply.github.com>
2019-12-18 12:02:28 +0530

    Add files via upload

---
+++ Divisible by 5 and 11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	if(n%5==0&&n%11==0)
+	printf("The number is divisible by both 5 and 11\n");
+	else
+	printf("The number is not divisible by 5 and 11\n");
+	return 0;
+}
